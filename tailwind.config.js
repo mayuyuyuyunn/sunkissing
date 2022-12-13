03442fbda3c8eaa697f5bd923059f1d9c0af01c1
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2022-12-13 22:28:01 +0900

    Add: slim対応へ変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
+    './app/views/**/*.html.slim',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
