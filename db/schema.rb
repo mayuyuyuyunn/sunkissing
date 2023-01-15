9d9f51cadaa01f7779e1878146851edc5ff1682d
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-01-15 17:18:16 +0900

    Add: Userテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_28_053105) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_15_081442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_28_053105) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_tags_on_name", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "line_user_id", null: false
+    t.integer "role", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "sunscreen_tags", "sunscreens"
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    line_user_id { "MyString" }
+    role { 1 }
+  end
+end
