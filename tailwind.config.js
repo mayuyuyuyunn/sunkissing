7ee0b2b888ec7430e02b6d183523d633695c4542
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-01-20 23:09:12 +0900

    Change: slimからerbに戻す

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: [
-    './app/views/**/*.html.slim',
+    './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
