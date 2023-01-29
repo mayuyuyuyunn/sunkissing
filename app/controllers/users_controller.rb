72883c3714beec23e83a9e6f4e73d4c39c323044
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-01-29 17:47:51 +0900

    Add: User登録・認証機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
-  def new
-  end
+  require 'net/http'
+  require 'uri'
+
+  def new; end
 
   def create
+    id_token = params[:idToken]
+    channel_id = 1657853635
+    res = Net::HTTP.post_form(URI.parse('https://api.line.me/oauth2/v2.1/verify'), {'id_token'=>id_token, 'client_id'=>channel_id})
+    line_user_id = JSON.parse(res.body)["sub"]
+    user = User.find_by(line_user_id: line_user_id)
+    if user.nil?
+      user = User.create(line_user_id: line_user_id)
+    end
+    session[:user_id] = user.id
+    render :json => user
   end
 end
