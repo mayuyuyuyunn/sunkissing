f860eb0fc6b32b99738c6f4b21126f40773de3ae
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-01-30 14:49:21 +0900

    Add: ユーザー登録・認証作成

---
+++ app/javascript/users/new.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // csrf-tokenを取得
+  const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+  const LIFF_ID = "XXXXXXXXXXXXXXXX";
+
+  liff.init({
+    liffId: LIFF_ID
+  })
+  .then(() => {
+    if (!liff.isLoggedIn()) {
+      liff.login()
+    }
+  })
+  .then(() => {
+    const idToken = liff.getIDToken()
+    console.log(idToken)
+    const body =`idToken=${idToken}`
+    const request = new Request('/users', {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+        'X-CSRF-Token': token
+      },
+      method: 'POST',
+      body: body
+    });
+
+    fetch(request)
+    .then(response => response.json())
+    .then(data => {
+      data_id = data
+    })
+  })
+  .then(() => {
+    if (!liff.isLoggedIn()) {
+      liff.login();
+    }
+  })
+})
