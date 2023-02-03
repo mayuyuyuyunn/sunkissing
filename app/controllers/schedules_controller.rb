34948dca539f9cf582685bdc7a5b374ebb6d7f62
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-02-03 10:28:09 +0900

    Add: スケジュール登録機能作成

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,8 +1,18 @@
 class SchedulesController < ApplicationController
+  before_action :login_required
+  
   def new
+    @schedule = current_user.schedules.new
+    @odekake_types = OdekakeType.all
   end
 
   def create
+    @schedule = current_user.schedules.build(schedule_params)
+    if @schedule.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -15,5 +25,11 @@ class SchedulesController < ApplicationController
   end
 
   def show
+  end
+
+  private
+  
+  def schedule_params
+    params.require(:schedule).permit(:leave_home_time, :odekake_type_id).merge(user_id: current_user.id)
   end
 end
