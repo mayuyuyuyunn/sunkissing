6826c12daf9321eb0d323c85a6e4db2217f4a4d7
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-02-22 20:32:20 +0900

    Add: JSでログイン周り実験

---
+++ app/javascript/users/edit.js
@@ -0,0 +1,33 @@
+document.getElementById("line_login").onclick = function() {
+  // csrf-tokenを取得
+  const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+  const LIFF_ID = "1657853635-5P9JrXx3";
+
+  liff.init({
+    liffId: LIFF_ID
+  })
+  liff.ready.then(() => {
+    if (!liff.isLoggedIn()) {
+      liff.login()
+    }
+  })
+  liff.ready.then(() => {
+    const idToken = liff.getIDToken()
+    console.log(idToken)
+    const body =`idToken=${idToken}`
+    const request = new Request('/users', {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+        'X-CSRF-Token': token
+      },
+      method: 'POST',
+      body: body
+    });
+
+    fetch(request)
+    .then(response => response.json())
+    .then(data => {
+      data_id = data
+    })
+  })
+}
