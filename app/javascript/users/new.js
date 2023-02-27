16738290da569a10d2f91db3e4c70ed93e972e16
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-02-27 17:14:30 +0900

    Add: herokuでのJS挙動を確かめる

--- app/javascript/users/new.js
+++ app/javascript/users/new.js
@@ -1,1 +1,35 @@
-console.log('test');
+console.log("custom as application")
+document.addEventListener('DOMContentLoaded', () => {
+  // csrf-tokenを取得
+  const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+  const LIFF_ID = "1657853635-5P9JrXx3";
+
+  liff
+  .init({
+    liffId: LIFF_ID,
+    // 他のブラウザで開いたときは初期化と一緒にログインもさせる
+    withLoginOnExternalBrowser: true
+  })
+  liff.ready.then(() => {
+    const idToken = liff.getIDToken()
+    console.log(idToken)
+    const body =`idToken=${idToken}`
+    const request = new Request('/users', {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+        'X-CSRF-Token': token
+      },
+      method: 'POST',
+      body: body
+    });
+
+    fetch(request)
+    .then(response => response.json())
+    .then(data => {
+      data_id = data
+    })
+    .then(() => {
+      window.location = '/after_login'
+    })
+  })
+})
