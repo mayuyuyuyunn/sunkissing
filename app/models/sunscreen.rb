ef3fa1dd3b88e55a1536621e4cf8c9f1664da564
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-04-19 18:46:02 +0900

    Add: 紫外線情報をAPIで取ってくる

--- app/models/sunscreen.rb
+++ app/models/sunscreen.rb
@@ -22,7 +22,7 @@ class Sunscreen < ApplicationRecord
   has_many :sunscreen_tags, dependent: :destroy
   has_many :tags, through: :sunscreen_tags
 
-  enum :spf, { spf_unknown: 0, thirty: 30, forty: 40, fifty: 50, fifty_plus: 55 }
+  enum :spf, { spf_unknown: 0, fifteen: 15, thirty: 30, forty: 40, fifty: 50, fifty_plus: 55 }
   enum :pa, { pa_unknown: 0, plus1: 1, plus2: 2, plus3: 3, plus4: 4 }
 
   validates :name, presence: true, uniqueness: true
