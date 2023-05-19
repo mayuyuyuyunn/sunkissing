90e044c1e8b7dcfa6b5a6229db78291d6d3eb134
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-05-19 15:45:12 +0900

    add: admin/schedules関連作成

--- app/controllers/admin/schedules_controller.rb
+++ app/controllers/admin/schedules_controller.rb
@@ -1,5 +1,24 @@
-class Admin::SchedulesController < Admin::applicationController
+class Admin::SchedulesController < Admin::ApplicationController
+  before_action :set_schedule, only: %i[show destroy]
+
   def index
     @schedules = Schedule.all
+  end
+
+  def show; end
+
+  def destroy
+    @schedule.destroy!
+    redirect_to admin_schedules_path, success: t('defaults.message.deleted', item: Schedule.model_name.human)
+  end
+
+  private
+
+  def set_schedule
+    @schedule = Schedule.find(params[:id])
+  end
+
+  def schedule_params
+    params.require(:schedule).permit(:leave_home_time)
   end
 end
