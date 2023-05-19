e266cd5f67dcf178fb3889bbb294145e69456b42
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-05-19 15:49:47 +0900

    Add: admin/sunscreen関連作成

--- app/controllers/admin/sunscreens_controller.rb
+++ app/controllers/admin/sunscreens_controller.rb
@@ -1,17 +1,18 @@
-class Admin::SunscreensController < Admin::applicationController
+class Admin::SunscreensController < Admin::ApplicationController
   before_action :set_sunscreen, only: %i[edit update show destroy]
 
   def new
-    @suncreen = Sunscreen.new
+    @sunscreen = Sunscreen.new
   end
 
   def create
-    if @suncreen.save(sunscreen_params)
+    @sunscreen = Sunscreen.new(sunscreen_params)
+    if @sunscreen.save
       flash[:success] = t('defaults.message.created', item: Sunscreen.model_name.human)
-      redirect_to root_path
+      redirect_to admin_sunscreens_path
     else
-      render :edit
       flash.now[:error] =  t('defaults.message.not_created', item: Sunscreen.model_name.human)
+      render :new
     end
   end
 
@@ -23,7 +24,7 @@ class Admin::SunscreensController < Admin::applicationController
 
   def update
     if @sunscreen.update(sunscreen_params)
-      redirect_to admin_sunscreen_path(@sunscreens), success: t('defaults.message.updated', item: Sunscreen.model_name.human)
+      redirect_to admin_sunscreens_path, success: t('defaults.message.updated', item: Sunscreen.model_name.human)
     else
       flash.now[:error] = t('defaults.message.not_updated', item: Sunscreen.model_name.human)
       render :edit
@@ -44,6 +45,6 @@ class Admin::SunscreensController < Admin::applicationController
   end
 
   def sunscreen_params
-    params.require(:sunscreen).permit(:name, :price, :spf, :pa, :summary, :brand, :manufacture, :capacity, :rakuten_url, :rakten_image_url)
+    params.require(:sunscreen).permit(:name, :spf, :pa, :capacity, :price, :manufacture, :brand, :summary, :rakuten_url, :rakuten_image_url)
   end
 end
