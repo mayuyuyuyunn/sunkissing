d2a4218c9551f28f42510544b205b8fe881d4340
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-05-19 15:41:22 +0900

    Add: admin/tag関連作成

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,17 +1,18 @@
-class Admin::TagsController < Admin::applicationController
+class Admin::TagsController < Admin::ApplicationController
   before_action :set_tag, only: %i[edit update show destroy]
 
   def new
-    @suncreen = Tag.new
+    @tag = Tag.new
   end
 
   def create
-    if @suncreen.save(tag_params)
+    @tag = Tag.new(tag_params)
+    if @tag.save
       flash[:success] = t('defaults.message.created', item: Tag.model_name.human)
-      redirect_to root_path
+      redirect_to admin_tags_path
     else
-      render :edit
       flash.now[:error] =  t('defaults.message.not_created', item: Tag.model_name.human)
+      render :new
     end
   end
 
@@ -23,7 +24,7 @@ class Admin::TagsController < Admin::applicationController
 
   def update
     if @tag.update(tag_params)
-      redirect_to admin_tag_path(@tags), success: t('defaults.message.updated', item: Tag.model_name.human)
+      redirect_to admin_tags_path, success: t('defaults.message.updated', item: Tag.model_name.human)
     else
       flash.now[:error] = t('defaults.message.not_updated', item: Tag.model_name.human)
       render :edit
