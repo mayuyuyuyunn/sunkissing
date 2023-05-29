7fb45e8393b4cb39901a9db9fc43275d5a298c46
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-05-29 15:00:38 +0900

    Refactor: lineのユーザー認証をserviceクラスに切り分けた

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,16 +11,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    id_token = params[:idToken]
-    channel_id = '1657853635'
-    res = Net::HTTP.post_form(URI.parse('https://api.line.me/oauth2/v2.1/verify'), {'id_token'=>id_token, 'client_id'=>channel_id})
-    line_user_id = JSON.parse(res.body)["sub"]
-    user = User.find_by(line_user_id: line_user_id)
+    line_user_authenticate = LineUserAuthenticateService.new(params[:idToken])
+    line_user_authenticate.call
+    user = line_user_authenticate.search_user
     if user.nil?
-      user = User.create(line_user_id: line_user_id)
-    elsif
+      user = User.create(line_user_id: line_user_authenticate.line_user_id)
       session[:user_id] = user.id
-      render :json => user
+      render json: user
+    elsif user
+      session[:user_id] = user.id
+      render json: user
     end
   end
 
