524d6fb162712d54569e22e67a371921de24f913
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-06-16 11:20:33 +0900

    Add: Userのモデルスペックを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,5 +23,75 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    it 'line_user_idが存在する場合、有効であること' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it 'line_user_idが重複する場合、無効であること' do
+      create(:user, line_user_id: 'test_user_id')
+      user = build(:user, line_user_id: 'test_user_id')
+      expect(user).not_to be_valid
+      expect(user.errors[:line_user_id]).to include('はすでに存在します')
+    end
+
+    it 'ageが存在する場合、有効であること' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it 'ageが存在しない場合、無効であること' do
+      user = build(:user, age: nil )
+      expect(user).not_to be_valid
+    end
+
+    it 'genderが存在する場合、有効であること' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it 'genderが存在しない場合、無効であること' do
+      user = build(:user, gender: nil )
+      expect(user).not_to be_valid
+    end
+
+    it 'skin_typeが存在する場合、有効であること' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it 'skin_typeが存在しない場合、無効であること' do
+      user = build(:user, skin_type: nil )
+      expect(user).not_to be_valid
+    end
+
+    it 'prefecture_idが存在する場合、有効であること' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it 'prefecture_idが存在しない場合、無効であること' do
+      user = build(:user, prefecture_id: nil )
+      expect(user).not_to be_valid
+    end
+  end
+
+  describe 'enum' do
+    it 'roleが正しく定義されていること' do
+      expect(User.roles.keys).to contain_exactly('general', 'admin')
+    end
+
+    it 'ageが正しく定義されていること' do
+      expect(User.ages.keys).to contain_exactly('age_not_selected', 'teen', 'twenty', 'thirty', 'forty', 'fifty', 'sixth', 'age_other')
+    end
+
+    it 'genderが正しく定義されていること' do
+      expect(User.genders.keys).to contain_exactly('gender_not_selected', 'male', 'female', 'gender_other')
+    end
+
+    it 'skin_typeが正しく定義されていること' do
+      expect(User.skin_types.keys).to contain_exactly('normal', 'oily', 'dry', 'combination', 'sensitive')
+    end
+  end
 end
